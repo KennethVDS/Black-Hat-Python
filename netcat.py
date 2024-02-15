d342e7a0325c38f0b7b2049cd6c7032701d6f95b
Knnth <49682872+KennethVDS@users.noreply.github.com>
2024-02-15 16:28:13 +0100

    episode 5

--- netcat.py
+++ netcat.py
@@ -13,6 +13,7 @@ def execute(cmd):
     output = subprocess.check_output(shlex.split(cmd) ,stderr=subprocess.STDOUT)
     return output.decode()
 
+
 class NetCat:
     def __init__(self, args, buffer = None):
         self.args = args
@@ -23,35 +24,82 @@ class NetCat:
     def run(self):
         if self.args.listen:
             self.listen()
-        else
+        else:
             self.send()
     
+
     def send(self):
         self.socket.connect((self.args.target, self.args.port))
         if self.buffer:
             self.socket.send(self.buffer)
             try:
                 while True:
-                    recv len = 1
-                    response : ''
-                    while recv len:
+                    recv_len = 1
+                    response = ''
+                    while recv_len:
                         data = self.socket.recv(4096)
-                        recv len = len(data)
+                        recv_len = len(data)
                         response += data.decode()
-                        if recv len < 4096
+                        if recv_len < 4096:
                             break
                         if response:
                             print(response)
                             buffer = input('>')
                             buffer += '\n'
-                            self.socket.send(buffer+.encode()
-                except KeyboardInterrupt:
+                            self.socket.send(buffer.encode())
+            except KeyboardInterrupt:
                     print('User terminated.')
                     self.socket.close()
-                    sys.exit())
-                            
+                    sys.exit()
+    
 
-if __name__ == '__main__':
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(target=self.handle, args=(client_socket,))
+            client_thread.start()
+
+
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+
+        elif self.args.upload:
+            file_buffer=b''
+            while True:
+                data = client_socket.recv(4096)
+                if data: 
+                    file_buffer += data
+                    print(len(file_buffer))
+                else:
+                    break
+            
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+            message = f'Saved file {self.args.upload}'
+            client_socket.send(message.encode())
+
+        elif self.args.command:
+            cmd_buffer = b''
+            while True:
+                try:
+                    client_socket.send(b' #> ')
+                    while '\n' not in cmd_buffer.decode():
+                        cmd_buffer += client_socket.recv(64)
+                    response = execute(cmd_buffer.decode())
+                    if response:
+                        client_socket.send(response.encode())
+                    cmd_buffer = b''
+                except Exception as e:
+                    print(f'server killed {e}')
+                    self.socket.close()
+                    sys.exit()
+
+
+if  __name__  ==  '__main__':
     parser = argparse.ArgumentParser(
         description='BHP Net Tool',
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -69,3 +117,11 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
     parser.add_argument('-u', '--upload', help='upload file')
     args = parser.parse_args()
+
+    if args.listen:
+        buffer =''
+    else:
+        buffer = sys.stdin.read()
+    
+    nc = NetCat(args, buffer.encode())
+    nc.run()
